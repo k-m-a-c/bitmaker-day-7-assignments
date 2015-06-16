eca6b6fc92b434166ce6c3a76bbbc284236e3eb8
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-06-16 13:29:25 -0400

    add rolodex and add contact feature

--- crm.rb
+++ crm.rb
@@ -1,5 +1,8 @@
 require_relative 'contact.rb'
+require_relative 'rolodex.rb'
 require 'sinatra'
+
+$rolodex = Rolodex.new
 
 get "/" do
   @crm_app_name = "Kevin's CRM"
@@ -8,30 +11,26 @@ end
 
 get "/contacts" do
   @contacts_page_title = "My Contacts"
-  @contacts = []
-  @contacts << Contact.new("Jacob", "Dawson", "jdawg@gmail.com", "Lunatic.")
-  @contacts << Contact.new("Jill", "Cates", "JC@gmail.com", "Sexy yogi.")
-  @contacts << Contact.new("Alex", "Warme", "awarme@gmail.com", "Asshat.")
 
   erb :contacts
 end
 
 get "/contacts/delete" do
   @delete_contact_page_title = "Delete a Contact"
-  erb :delete
+  erb :delete_contact
 end
 
 get "/contacts/edit" do
   @edit_page_title = "Edit a Contact"
-  erb :edit
+  erb :edit_contact
 end
 
 get "/contacts/add" do
   @add_contact_page_title = "Add a Contact"
-  erb :add
+  erb :add_contact
 end
 
 get "/contacts/show" do
   @contact_attribute_page_title = "See a Contact's Attributes"
-  erb :show
+  erb :show_contact
 end
