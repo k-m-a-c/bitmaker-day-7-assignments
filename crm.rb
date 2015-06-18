261f8f314b66f67b27522af6c12c163e8f237117
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-06-18 14:45:03 -0400

    fat nav/header effect achieved

--- crm.rb
+++ crm.rb
@@ -8,43 +8,37 @@ $rolodex = Rolodex.new
 
 get "/" do
   @page_title = "Home | Kevin's CRM"
-  @page_h1 = "Kevin's CRM"
-  @page_h2 = "Main Menu"
+  @page_h1 = "Main Menu"
   erb :index
 end
 
 get "/contacts" do
   @page_title = "Contacts | Kevin's CRM"
   @page_h1 = "Your Contacts"
-  @page_h2 = "Here's your list of contacts:"
   erb :contacts
 end
 
 get "/contacts/delete" do
   @page_title = "Delete a Contact | Kevin's CRM"
   @page_h1 = "Delete a Contact"
-  @page_h2 = "Select a contact to delete:"
   erb :delete_contact
 end
 
 get "/contacts/edit" do
   @page_title = "Edit a Contact | Kevin's CRM"
   @page_h1 = "Edit a Contact"
-  @page_h2 = "Select a contact to edit:"
   erb :edit_contact
 end
 
 get "/contacts/add" do
   @page_title = "Add a Contact | Kevin's CRM"
   @page_h1 = "Add a Contact"
-  @page_h2 = "Type in a contact's info to create a new contact:"
   erb :add_contact
 end
 
 get "/contacts/show" do
   @page_title = "Show a Contact | Kevin's CRM"
-  @page_h1 = "See your Contacts' Attributes"
-  @page_h2 = "Here's an attribute of your contact:"
+  @page_h1 = "Find a Contact"
   erb :show_contact
 end
 
